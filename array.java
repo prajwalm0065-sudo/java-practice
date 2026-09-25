0441483c2ad2e06ab214149be7f8997f00ed1353
Prajwal M <prajwalm0065@gmail.com>
2026-09-25 12:14:22 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,8 @@
+public class array{
+    public static void main(String args[])
+    {
+        int []arr={1,2,3};
+        double[]documents=(double[])arr;
+        System.out.println(documents[0]);
+    }
+}
